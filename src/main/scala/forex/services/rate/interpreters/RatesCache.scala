f6022a180ff43d6b5c79ad0ee39cb75344e422ca
Gregory Rozhkov <grigoriyroghkov@gmail.com>
2023-05-25 01:21:44 +0300

    Remove verbose read-write lock

--- src/main/scala/forex/services/rate/interpreters/RatesCache.scala
+++ src/main/scala/forex/services/rate/interpreters/RatesCache.scala
@@ -1,9 +1,9 @@
 package forex.services.rate.interpreters
 
-import cats.effect.{ Concurrent, Sync }
+import cats.effect.concurrent.Ref
+import cats.effect.Concurrent
+import cats.implicits._
 import forex.domain.Rate
-
-import java.util.concurrent.locks.ReentrantReadWriteLock
 
 sealed trait RatesCache[F[_]] {
   def get(key: Rate.Pair): F[Option[Rate]]
@@ -14,30 +14,13 @@ object RatesCache {
   def create[F[_]: Concurrent](): RatesCache[F] = new ConcurrentRatesCache[F]()
 }
 
-class ConcurrentRatesCache[F[_]: Concurrent](lock: ReentrantReadWriteLock = new ReentrantReadWriteLock())
-    extends RatesCache[F] {
+class ConcurrentRatesCache[F[_]: Concurrent]() extends RatesCache[F] {
 
-  private[this] var cache: Map[Rate.Pair, Rate] = Map.empty
+  private[this] val cache: Ref[F, Map[Rate.Pair, Rate]] = Ref.unsafe(Map.empty)
 
   override def get(key: Rate.Pair): F[Option[Rate]] =
-    Sync[F].delay {
-      val readLock = lock.readLock()
-      readLock.lock()
-      try {
-        cache.get(key)
-      } finally {
-        readLock.unlock()
-      }
-    }
+    cache.get.map(_.get(key))
 
   override def updateAll(update: Map[Rate.Pair, Rate]): F[Unit] =
-    Sync[F].delay {
-      val writeLock = lock.writeLock()
-      writeLock.lock()
-      try {
-        cache = update
-      } finally {
-        writeLock.unlock()
-      }
-    }
+    cache.set(update)
 }
